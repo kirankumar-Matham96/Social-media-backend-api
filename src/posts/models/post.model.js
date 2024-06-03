645c1a956f4638324da36a79e6c6345bdd8fbbca
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-06-03 17:10:07 +0530

    feat: added post model

---
+++ src/posts/models/post.model.js
@@ -0,0 +1,81 @@
+import { v4 as uuidv4 } from "uuid";
+
+const posts = [
+  {
+    id: 1,
+    userId: 1,
+    caption: "First caption",
+    imageUrl: "http://sampleurl1.com",
+  },
+  {
+    id: 2,
+    userId: 2,
+    caption: "Second caption",
+    imageUrl: "http://sampleurl2.com",
+  },
+];
+
+class PostModel {
+  constructor(userId, caption, imageUrl) {
+    this.id = uuidv4();
+    this.userId = userId;
+    this.caption = caption;
+    this.imageUrl = imageUrl;
+  }
+
+  static add = (userId, caption, imageUrl) => {
+    const newPost = new PostModel(userId, caption, imageUrl);
+    return { post: newPost, status: 200 };
+  };
+
+  static getAll = () => {
+    return { posts, status: 200 };
+  };
+
+  static get = (id) => {
+    const postFound = posts.find((post) => post.id === id);
+    if (!postFound) {
+      throw new Error("post not found", 404);
+    }
+
+    return { post: postFound, status: 200 };
+  };
+
+  static getUserPosts = (userId) => {
+    const userPosts = posts.filter((post) => post.userId === userId);
+    return { posts: userPosts, status: 200 };
+  };
+
+  static update = (postId, userId, caption, imageUrl) => {
+    const foundPost = posts.find((post) => post.id === postId);
+    if (!foundPost) {
+      throw new Error("post not found", 404);
+    }
+
+    if (postFound.userId !== userId) {
+      throw new Error("user not authorized", 403);
+    }
+
+    foundPost.caption = caption;
+    foundPost.imageUrl = imageUrl;
+
+    return { post: foundPost, status: 200 };
+  };
+
+  static delete = (postId, userId) => {
+    const foundPostIndex = posts.findIndex((post) => post.id === postId);
+    if (foundPostIndex == -1) {
+      throw new Error("post not found", 404);
+    }
+
+    if (posts[foundPostIndex].userId !== userId) {
+      throw new Error("user not authorized", 403);
+    }
+
+    const postDeleted = posts.splice(foundPostIndex, 1);
+
+    return { post: postDeleted, status: 200 };
+  };
+}
+
+export default PostModel;
