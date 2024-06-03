1ec2816c7a8aa7117153857433223f5d64f05d6a
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-06-03 19:08:25 +0530

    feat: added comment controller

---
+++ src/comments/controllers/comment.controller.js
@@ -0,0 +1,79 @@
+import CommentModel from "../models/comment.model.js";
+
+class CommentController {
+  createNewComment = (req, res) => {
+    const { userId } = req;
+    const { postId } = req.params;
+    const { content } = req.body;
+
+    if (!postId) {
+      throw new Error("post id is required", 400);
+    }
+
+    if (!content) {
+      throw new Error("content is required", 400);
+    }
+
+    const { comment, status } = CommentModel.add(userId, postId, content);
+    res.status(status).send({
+      status: "success",
+      message: "comment added successfully",
+      comment,
+    });
+  };
+
+  getAllCommentsRelatedToAPost = (req, res) => {
+    const { userId } = req;
+    const { postId } = req.params;
+
+    if (!postId) {
+      throw new Error("post id is required", 400);
+    }
+
+    const { comments, status } = CommentModel.add(userId, postId, content);
+    res.status(status).send({
+      status: "success",
+      message: "comments retrieved successfully",
+      comments,
+    });
+  };
+
+  updateComment = (req, res) => {
+    const { userId } = req;
+    const { postId } = req.params;
+    const { content } = req.body;
+
+    if (!postId) {
+      throw new Error("post id is required", 400);
+    }
+
+    if (!content) {
+      throw new Error("content is required", 400);
+    }
+
+    const { comment, status } = CommentModel.getAll(userId, postId, content);
+    res.status(status).send({
+      status: "success",
+      message: "comment updated successfully",
+      comment,
+    });
+  };
+
+  deleteComment = (req, res) => {
+    const { userId } = req;
+    const { postId } = req.params;
+
+    if (!postId) {
+      throw new Error("post id is required", 400);
+    }
+
+    const { comment, status } = CommentModel.delete(userId, postId);
+    res.status(status).send({
+      status: "success",
+      message: "comment deleted successfully",
+      comment,
+    });
+  };
+}
+
+export default CommentController;
