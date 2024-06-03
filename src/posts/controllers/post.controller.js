727037ae6299b7d7228e15a38b4b1e4084c8bf2e
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-06-03 17:56:06 +0530

    feat: added post controller

---
+++ src/posts/controllers/post.controller.js
@@ -0,0 +1,92 @@
+import PostModel from "../models/post.model";
+
+class PostController {
+  createPost = (req, res) => {
+    const { caption, imageUrl } = req.body;
+    const { userId } = req.userId;
+    if (!caption) {
+      throw new Error("caption is required", 400);
+    }
+    if (!imageUrl) {
+      throw new Error("image url is required", 400);
+    }
+
+    const { post, status } = PostModel.add(userId, caption, imageUrl);
+    res
+      .status(status)
+      .send({ status: "success", message: "post created successfully", post });
+  };
+
+  getAllPosts = (req, res) => {
+    const { posts, status } = PostModel.getAll();
+    res.status(status).send({
+      status: "success",
+      message: "posts retrieved successfully",
+      posts,
+    });
+  };
+
+  getPostById = (req, res) => {
+    const { postId } = req.params;
+    if (!postId) {
+      throw new Error("post id required", 400);
+    }
+
+    const { post, status } = PostModel.get(postId);
+    res.status(status).send({
+      status: "success",
+      message: "post retrieved successfully",
+      post,
+    });
+  };
+
+  getUserPosts = (req, res) => {
+    const { userId } = req.params;
+    const { posts, status } = PostModel.getUserPosts(userId);
+    res.status(status).send({
+      status: "success",
+      message: "posts retrieved successfully",
+      posts,
+    });
+  };
+
+  updatePost = (req, res) => {
+    const { postId } = req.params;
+    const { userId } = req;
+    const { caption, imageUrl } = req.body;
+
+    if (!caption) {
+      throw new Error("caption is required", 400);
+    }
+    if (!imageUrl) {
+      throw new Error("image URL is required", 400);
+    }
+    if (!postId) {
+      throw new Error("post id is required", 400);
+    }
+
+    const { post, status } = PostModel.update(
+      postId,
+      userId,
+      caption,
+      imageUrl
+    );
+    res
+      .status(status)
+      .send({ status: "success", message: "post updated successfully", post });
+  };
+
+  deletePost = (req, res) => {
+    const { postId } = req.params;
+    const { userId } = req;
+    if (!postId) {
+      throw new Error("post id is required");
+    }
+    const { post, status } = PostModel.delete(postId, userId);
+    res
+      .status(status)
+      .send({ status: "success", message: "post deleted successfully", post });
+  };
+}
+
+export default PostController;
